87ae3f537d708836b674cbfdfc66c071ab4ab7d4
meiseg <131057544+meiseg@users.noreply.github.com>
2023-04-18 18:58:54 -0400

    Update Junior_design.cpp

--- Junior_design.cpp
+++ Junior_design.cpp
@@ -1,132 +1,144 @@
-#include <gtkmm.h>
-#include <fstream>
 #include <iostream>
+#include <fstream>
+#include <gtkmm.h>
 
 using namespace std;
 
-void hide(Gtk::Box& box){
-	auto children = box.get_children();
-    for (auto& child : children) child->hide();
+class Junior {
+public:
+	// widgets create here
+    Gtk::Window window;
+    Gtk::Box box{Gtk::ORIENTATION_VERTICAL};
+    Gtk::Label label;
+	Gtk::MenuButton menuButton;
+	Gtk::Menu menu;
+	Gtk::Label label1{"label1"};
+	Gtk::Label label2{"label2"};
+	Gtk::MenuItem item1,item2;
+    Gtk::Button button1{"Karoake Mode"}, button2{"Warm Up"}, button3{"return"};
+
+    int state;
+
+	void set_state(int);
+    void hide() {
+        auto children = box.get_children();
+        for (auto &child : children)
+            child->hide();
 }
-void set_setup(Gtk::Window& window, Gtk::Button& button1,Gtk::Button& button2,Gtk::Button& button3,Gtk::Label& label,Gtk::Box& box,Gtk::Menu& item, Gtk::MenuButton& menu){
 
-window.set_default_size(500,500);
-	window.set_title("Junior Design Project");button1.set_size_request(250,250);
-	button2.set_size_request(250,250);
-	button3.set_size_request(50,50);
-	label.override_font(Pango::FontDescription("Arial 50"));
-	button3.set_valign(Gtk::ALIGN_END);
-	button3.set_halign(Gtk::ALIGN_START);
-	
-	Gtk::MenuItem item1("option 1"), item2("option2");
-	
-	Gtk::Popover as;
-	as.add(item1);
-	as.add(item2);
-	menu.set_popover(as);
-	menu.set_margin_start(10);
-	menu.set_margin_end(10);
-	menu.set_margin_top(10);
-	menu.set_margin_bottom(10);
-	
-	box.pack_start(menu,Gtk::PACK_SHRINK);
-	
-	box.pack_start(button1,Gtk::PACK_SHRINK);
-	box.pack_start(button2,Gtk::PACK_SHRINK);
-	box.pack_end(button3,Gtk::PACK_SHRINK);
-	box.set_center_widget(label);
-
-}
-void initial_display(Gtk::Window& window, Gtk::Button& button1,Gtk::Button& button2,Gtk::Button& button3,Gtk::Label& label,Gtk::Box& box){
-
-window.add(box);
-box.show();
-button1.show();
-button2.show();
-button3.hide();
-label.hide();
-
-
-}
-string read_ff(){fstream fs("file1.txt");
-			string a;
-			getline(fs,a);
-				   return a;}
-void live_feed(Gtk::Label& label,Gtk::Button& button3,int& state){
-
-	
-	 Glib::signal_timeout().connect([&](){	
-		if(button3.get_visible()){
-			
-		string a =read_ff();
+    void set_setup(){
 		
+		//set up for windows and title
+        
+		window.set_default_size(1920, 1000);
+        window.set_title("Junior Design Project");
+        
+		
+		//set up size for buttons and alignment
+		button1.set_size_request(250, 500);
+        button2.set_size_request(250, 500);
+        button3.set_size_request(50, 50); 
+		button1.override_font(Pango::FontDescription("Arial 50"));
+		button2.override_font(Pango::FontDescription("Arial 50"));
+        label.override_font(Pango::FontDescription("Arial 50"));
+        button3.set_valign(Gtk::ALIGN_END);
+        button3.set_halign(Gtk::ALIGN_START);
+		
+		//setup for menu button
+		item1.set_label("option 1");
+		item2.set_label("option2");
+		menu.append(item1);
+		menu.append(item2);
+		menu.set_halign(Gtk::ALIGN_CENTER);
+		menu.show_all();
+		menuButton.set_popup(menu);
+		menuButton.set_size_request(500, 20);
+		menuButton.set_direction(Gtk::ARROW_DOWN);
+		// adding items to containter
+		box.pack_start(menuButton, Gtk::PACK_SHRINK);
+		//box.pack_start(menuButton,Gtk::PACK_SHRINK);
+        box.pack_start(button1, Gtk::PACK_SHRINK);
+        box.pack_start(button2, Gtk::PACK_SHRINK);
+        box.pack_end(button3, Gtk::PACK_SHRINK);
+        box.set_center_widget(label);
+}
+
+    void initial_display() {
+        window.add(box);
+        box.show();
+        button1.show();
+        button2.show();
+        button3.hide();
+        label.hide();
+		//menuButton.hide();
+}
+
+    string read_ff() {
+        fstream fs("file1.txt");
+        string a;
+        getline(fs, a);
+        return a;
+}
+
+    void live_feed() {
+		Glib::signal_timeout().connect([&]() {
+		if (button3.get_visible()) {
+		string a = read_ff();
 		label.set_text(a);
-			if(state == 5) label.show();
-			else label.hide();
+		if (state == 5)
+		label.show();
+		else
+		label.hide();
 		}
-			
-		;return true;},10);
-	
+		return true;
+		}, 10);
 }
 
-void clicked_button3(Gtk::Button& button3,Gtk::Button& button1,Gtk::Button& button2,Gtk::Window& window,Gtk::Label& label,Gtk::Box& box,Gtk::MenuButton& menu){
-hide(box);
-button3.show();
-
-
-button3.signal_clicked().connect([&](){
-
-button1.show();
-button2.show();
-button3.hide();
-label.hide();
-menu.hide();
-window.set_default_size(500,500);
-
-});}
-void men(Gtk::MenuButton& menu){
-	menu.signal_clicked().connect([&]() {
-   
-});
-
+    void clicked_button3() {
+        hide();
+        button3.show();
+		if(state == 1) menuButton.show(); else menuButton.hide();
+        button3.signal_clicked().connect([&]() {
+		button1.show();
+		button2.show();
+		button3.hide();
+		label.hide();
+		menuButton.hide();
+		window.set_default_size(500, 500);
+		});
 }
 
-int main(int x, char ** a){
 
-	Gtk::Main kit (x,a);
-	auto state = 5;
 
-	Gtk::Window window;
-	Gtk::Box box(Gtk::ORIENTATION_VERTICAL);
-	Gtk::Label label;
-	Gtk::Menu item;
-	Gtk::MenuButton menu;
+  
+};
+void Junior::set_state(int x){(*this).state = x;}
+
+
+	int main(int x, char ** v){
 	
-	Gtk::Button button1( "Karoake Mode"),button2("Warm Up"),button3("return");
+	Gtk::Main kit (x,v);
+	Junior runt;
+	runt.set_setup();
+runt.button2.signal_clicked().connect([&](){
+	runt.set_state(5);
+	runt.live_feed();
 
-	set_setup(window,button1,button2,button3,label,box,item,menu);
-
-button2.signal_clicked().connect([&](){
-	state = 5;
-	live_feed(label,button3,state);
-
-	clicked_button3(button3,button1,button2,window,label,box,menu);
+	runt.clicked_button3();
 });
 
-button1.signal_clicked().connect([&](){
-	state =1;
-	live_feed(label,button3,state);	
-	
+runt.button1.signal_clicked().connect([&](){
+	runt.set_state(1);
+	runt.live_feed();	
 
-	clicked_button3(button3,button1,button2,window,label,box,menu);
-	item.show();
+	runt.clicked_button3();
 	
-	menu.show();
-	men(menu);
 });
 
-	initial_display(window,button1,button2,button3,label,box);
-	Gtk::Main::run(window);
+	runt.initial_display();
+	Gtk::Main::run(runt.window);
 
 	return 0;
+
+
 }
